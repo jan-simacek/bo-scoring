c2d072982ed38768aaf480724ff753a40a546d48
Jan Simacek <jsimacek@essentialaccessibility.com>
2023-06-16 08:17:22 +0200

    initial commit

---
+++ forge.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  packagerConfig: {
+    icon: 'icons/icon.png'
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        icon: path.resolve(__dirname, "icons/icon.png")
+      },
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
